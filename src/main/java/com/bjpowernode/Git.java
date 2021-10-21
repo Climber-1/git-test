1b616c8594fc3cb45691d8e4c07fbac0306178d8
zhm <2351298547@qq.com>
2021-10-21 19:31:16 +0800

    push commit

--- src/main/java/com/bjpowernode/Git.java
+++ src/main/java/com/bjpowernode/Git.java
@@ -3,5 +3,10 @@ package com.bjpowernode;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("push test");
     }
 }
